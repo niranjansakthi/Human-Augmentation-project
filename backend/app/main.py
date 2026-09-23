e9841d7c14a294affd17eef32343e889bb81c965
niranjansakthi <niranjan79046@gmail.com>
2026-09-23 14:51:43 +0530

    Fix CORS error for Vercel deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -74,6 +74,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "https://human-augmentation-project.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
